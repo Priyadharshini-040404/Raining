7cb1db8f99ca9bcce5d80c01fb2c534c592afacb
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 22:14:51 +0530

    umbrella

--- main.cpp
+++ main.cpp
@@ -14,6 +14,13 @@ int main() {
         if (raining == 'y' || raining == 'Y') {
             cout << "Did you find an umbrella? (y/n): ";
             cin >> umbrella;
+            if (umbrella == 'y' || umbrella == 'Y') {
+                cout << "Bring the umbrella." << endl;
+                programOn = false; // End program
+            } else {
+                cout << "No umbrella found. Checking again..." << endl;
+                // Loop continues → check rain again
+            }
         }
     }
     cout << "\nProgram Closed." << endl;
