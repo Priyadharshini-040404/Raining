aed82a9cad4835cc72e6dec1e87222c609fc57c3
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 22:11:00 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    char raining, umbrella;
+    bool programOn = true;
+ 
+    cout << "=== Umbrella Finder Program ===" << endl;
+ 
+    
+ 
+    cout << "\nProgram Closed." << endl;
+    return 0;
+}
