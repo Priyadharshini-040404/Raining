0dfbe43ec478e3c8fde09b6d394249ea0ab6fc50
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 22:13:45 +0530

    rain

--- main.cpp
+++ main.cpp
@@ -7,8 +7,15 @@ int main() {
  
     cout << "=== Umbrella Finder Program ===" << endl;
  
-    
+    while (programOn) {
+        cout << "\nIs it raining? (y/n): ";
+        cin >> raining;
  
+        if (raining == 'y' || raining == 'Y') {
+            cout << "Did you find an umbrella? (y/n): ";
+            cin >> umbrella;
+        }
+    }
     cout << "\nProgram Closed." << endl;
     return 0;
 }
